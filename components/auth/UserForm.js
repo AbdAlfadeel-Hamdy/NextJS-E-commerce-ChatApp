3c5d04aa9a1f0a361b37e8261c951488752fba25
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-03-09 01:14:23 +0200

    button

--- components/auth/UserForm.js
+++ components/auth/UserForm.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 const UserForm = () => {
+  const [isLoggingIn, setIsLoggingIn] = useState(true);
   return (
-    <section className="h-96 flex justify-center items-center ">
+    <section className="min-h-96 flex justify-center items-center pt-12 ">
       <form className="bg-blue-600 flex flex-col gap-4 items-center w-4/5 md:w-1/4 py-8 px-16 rounded-lg text-white">
         <h2 className="text-2xl">Join Us!</h2>
         <div className="flex flex-col items-center gap-2">
@@ -25,8 +28,16 @@ const UserForm = () => {
             className="p-2 outline-none caret-blue-600 text-blue-600 text-lg rounded-md"
           />
         </div>
-        <div>
-          <button>Log In</button>
+        <div className="w-full flex justify-center flex-col items-center gap-2">
+          <button className="bg-white  text-blue-600 py-2 px-4 w-3/4 rounded-md mt-4 text-lg font-bold hover:bg-blue-100 duration-300">
+            {isLoggingIn ? "Log in" : "Sign Up"}
+          </button>
+          <span
+            onClick={setIsLoggingIn.bind(this, !isLoggingIn)}
+            className="cursor-pointer"
+          >
+            Switch
+          </span>
         </div>
       </form>
     </section>

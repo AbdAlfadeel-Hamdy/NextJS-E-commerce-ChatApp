aecc17d8dca5b5ccc7fe045268ef993dda7cfa17
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-11 04:32:24 +0200

    meta data

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,11 +1,19 @@
 import SignForm from "@/components/CTA/SignForm";
 import { SignContextProvider } from "@/context/sign-context";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const AuthPage = () => {
   return (
-    <SignContextProvider>
-      <SignForm />
-    </SignContextProvider>
+    <Fragment>
+      <Head>
+        <title>Login | Signup</title>
+        <meta name="desciption" content="Verify user to enjoy shopping" />
+      </Head>
+      <SignContextProvider>
+        <SignForm />
+      </SignContextProvider>
+    </Fragment>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
 const HomePage = () => {
-  return <div>All Products</div>;
+  return (
+    <Fragment>
+      <Head>
+        <title>Gonzalo's Store - Find All You Need</title>
+        <meta
+          name="description"
+          content="An online store to sell customers' products"
+        />
+      </Head>
+      <div>All Products</div>
+    </Fragment>
+  );
 };
 
 export default HomePage;
